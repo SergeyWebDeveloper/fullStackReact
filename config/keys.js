d3be721331aac5e85851de117c6b21e4407bea01
Sergey Kozlov <sergeykozlov.2011@yandex.ru>
2018-04-19 20:30:58 +0300

    add jwt

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb://sergey:sergey@ds151169.mlab.com:51169/dev-connector'
+	mongoURI: 'mongodb://sergey:sergey@ds151169.mlab.com:51169/dev-connector',
+	secretOrKey: 'secret'
 };
