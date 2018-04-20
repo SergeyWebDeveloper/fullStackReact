f19667e584c929cdef26d768d66e8ac3a6848ecd
Sergey Kozlov <sergeykozlov.2011@yandex.ru>
2018-04-20 22:47:05 +0300

    add profile in route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
 
 // @route GET api/profile/test
 router.get('/test', (req, res) => res.json({msg: "Profile Works"}));
+
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+	const errors={};
+	Profile.findOne({user: req.user.id})
+		.then(profile=>{
+			if(!profile){
+				errors.noprofile = 'Профиль не найден';
+				return res.status(404).json(errors);
+			}
+			res.json(profile);
+		})
+		.catch(err=>res.status(404).json(err))
+});
 
 module.exports = router;
