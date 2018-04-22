9ab48a73fdf519d3807a58bc2ff9dfb2d75d5e07
Sergey Kozlov <sergeykozlov.2011@yandex.ru>
2018-04-22 18:56:41 +0300

    add and remove likes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -64,4 +64,47 @@ router.delete(
 	}
 );
 
+// @route post api/posts/like/:id
+router.post(
+	'/like/:id',
+	passport.authenticate('jwt', {session: false}),
+	(req, res) => {
+		Profile.findOne({user: req.user.id})
+			.then(profile => {
+				Post.findById(req.params.id)
+					.then(post => {
+						if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+							return res.status(400).json({alreadyliked: 'Вам уже нравится этот пост'});
+						}
+						post.likes.unshift({user: req.user.id});
+						post.save().then(post => res.json(post));
+					})
+					.catch(() => res.status(404).json({postnotfound: 'Пост не найден'}))
+			});
+	}
+);
+
+// @route post api/posts/unlike/:id
+router.post(
+	'/unlike/:id',
+	passport.authenticate('jwt', {session: false}),
+	(req, res) => {
+		Profile.findOne({user: req.user.id})
+			.then(profile => {
+				Post.findById(req.params.id)
+					.then(post => {
+						if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+							return res.status(400).json({notliked: 'Вам еще не понравился этот пост'});
+						}
+						const removeIndex = post.likes
+							.map(item => item.user.toString())
+							.indexOf(req.user.id);
+						post.likes.splice(removeIndex, 1);
+						post.save().then(post => res.json(post));
+					})
+					.catch(() => res.status(404).json({postnotfound: 'Пост не найден'}))
+			});
+	}
+);
+
 module.exports = router;
