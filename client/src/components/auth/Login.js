332c7e31f8f2eb56ae53c858cf54dfbd0179db45
Sergey Kozlov <sergeykozlov.2011@yandex.ru>
2018-04-24 23:55:00 +0300

    show errors in register form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,59 @@
 import React, {Component} from 'react';
 
 class Login extends Component {
+	state = {
+		email: '',
+		password: '',
+		errors: {}
+	};
+
+	onChange = (e) => {
+		this.setState({[e.target.name]: e.target.value});
+	};
+
+	onSubmit = (e) => {
+		e.preventDefault();
+		const user = {
+			email: this.state.email,
+			password: this.state.password,
+		};
+		console.log(user);
+	};
+
 	render() {
 		return (
-			<div>
-				<h1>Логин</h1>
+			<div className="login">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-8 m-auto">
+							<h1 className="display-4 text-center">Логин</h1>
+							<p className="lead text-center">Войти в аккаунт</p>
+							<form onSubmit={this.onSubmit}>
+								<div className="form-group">
+									<input
+										type="email"
+										className="form-control form-control-lg"
+										placeholder="Email адрес"
+										name="email"
+										onChange={this.onChange}
+										value={this.state.email}
+									/>
+								</div>
+								<div className="form-group">
+									<input
+										type="password"
+										className="form-control form-control-lg"
+										placeholder="Пароль"
+										name="password"
+										onChange={this.onChange}
+										value={this.state.password}
+									/>
+								</div>
+								<input type="submit" className="btn btn-info btn-block mt-4"/>
+							</form>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
