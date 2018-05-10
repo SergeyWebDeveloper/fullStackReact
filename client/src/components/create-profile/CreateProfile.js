a487ed3be3fd22db36d79f5024f3a95edc4699ba
Sergey Kozlov <sergeykozlov.2011@yandex.ru>
2018-05-10 20:34:11 +0300

    create profile

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import InputGroup from '../common/InputGroup';
 import SelectListGroup from '../common/SelectListGroup';
+import {createProfile} from '../../actions/profileAction';
 
 class CreateProfile extends Component {
 	state = {
@@ -29,9 +31,30 @@ class CreateProfile extends Component {
 		this.setState({[e.target.name]: e.target.value})
 	};
 
+	componentWillReceiveProps(nextProps){
+		if(nextProps.errors){
+			this.setState({errors: nextProps.errors})
+		}
+	}
+
 	onSubmit = (e) => {
 		e.preventDefault();
-		console.log('submit');
+		const profileData = {
+			handle: this.state.handle,
+			company: this.state.company,
+			website: this.state.website,
+			location: this.state.location,
+			status: this.state.status,
+			skills: this.state.skills,
+			githubusername: this.state.githubusername,
+			bio: this.state.bio,
+			twitter: this.state.twitter,
+			facebook: this.state.facebook,
+			linkedin: this.state.linkedin,
+			youtube: this.state.youtube,
+			instagram: this.state.instagram
+		};
+		this.props.createProfile(profileData,this.props.history);
 	};
 
 	showAndHideSocialIcons = () => {
@@ -183,6 +206,7 @@ class CreateProfile extends Component {
 								/>
 								<div className="mb-3">
 									<button
+										type='button'
 										className="btn btn-light"
 										onClick={this.showAndHideSocialIcons}
 									>
@@ -215,4 +239,4 @@ const mapStateToProps = state => ({
 	errors: state.errors
 });
 
-export default connect(mapStateToProps, null)(CreateProfile);
+export default connect(mapStateToProps, {createProfile})(withRouter(CreateProfile));
