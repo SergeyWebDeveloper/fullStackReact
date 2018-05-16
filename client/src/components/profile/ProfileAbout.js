b8d6eeebb2c4c785a32c38aa7ed6fe7ea22798d2
Sergey Kozlov <sergeykozlov.2011@yandex.ru>
2018-05-16 17:00:44 +0300

    render profile page experience and education

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,14 +1,39 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import isEmpty from "../../validation/is-empty";
 
 class ProfileAbout extends Component {
 	render() {
+		const {profile} = this.props;
+		const firstName = profile.user.name.trim().split(' ')[0];
+		const skills = profile.skills.map((skill)=>(
+			<div key={skill} className='p-3'>
+				<i className="fa fa-check" /> {skill}
+			</div>
+		));
+
 		return (
-			<div>
-				<h1>Profile about</h1>
+			<div className="row">
+				<div className="col-md-12">
+					<div className="card card-body bg-light mb-3">
+						<h3 className="text-center text-info">{firstName} биография</h3>
+						{isEmpty(profile.bio) ? <span>{firstName} не имеет биографии.</span> : (<p className="lead">{profile.bio}</p>)}
+						<hr />
+						<h3 className="text-center text-info">Навыки:</h3>
+						<div className="row">
+							<div className="d-flex flex-wrap justify-content-center align-items-center">
+								{skills}
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
 }
+
+ProfileAbout.propTypes = {
+	profile: PropTypes.object.isRequired
+};
 
 export default ProfileAbout;
--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,11 +1,52 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
 
 class ProfileCreds extends Component {
 	render() {
+		const {experience, education} = this.props;
+		const expItems = experience.map(exp => (
+			<li key={exp._id} className='list-group-item'>
+				<h4>{exp.company}</h4>
+				<p>
+					<Moment format='YYYY/MM/DD'>{exp.from}</Moment> -
+					{exp.to === null ? ' по НВ.' : <Moment format='YYYY/MM/DD'>{exp.to}</Moment>}
+				</p>
+				<p><strong>Позиция:</strong> {exp.title}</p>
+				{exp.location === '' ? null : (<p><strong>Местоположение: {exp.location}</strong></p>)}
+				{exp.description === '' ? null : (<p><strong>Описание: {exp.description}</strong></p>)}
+			</li>
+		));
+		const eduItems = education.map(edu => (
+			<li key={edu._id} className='list-group-item'>
+				<h4>{edu.school}</h4>
+				<p>
+					<Moment format='YYYY/MM/DD'>{edu.from}</Moment> -
+					{edu.to === null ? ' по НВ.' : <Moment format='YYYY/MM/DD'>{edu.to}</Moment>}
+				</p>
+				<p><strong>Степень:</strong> {edu.degree}</p>
+				<p><strong>Где учишься:</strong> {edu.fieldofstudy}</p>
+				{edu.description === '' ? null : (<p><strong>Описание: {edu.description}</strong></p>)}
+			</li>
+		));
 		return (
-			<div>
-				<h1>Profile creds</h1>
+			<div className='row'>
+				<div className="col-md-6">
+					<h3 className="text-center text-info">Опыт</h3>
+					{expItems.length>0 ? (
+						<ul className='list-group'>{expItems}</ul>
+					) :
+						<p className="text-center">Нет опыта</p>
+					}
+				</div>
+				<div className="col-md-6">
+					<h3 className="text-center text-info">Образование</h3>
+					{eduItems.length>0 ? (
+							<ul className='list-group'>{eduItems}</ul>
+						) :
+						<p className="text-center">Нет опыта</p>
+					}
+				</div>
 			</div>
 		);
 	}
